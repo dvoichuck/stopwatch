3c90dbeb78a3761d657f89766ab96b6a410170f7
dvoichuck <dvoichuck@student.ucode.world>
2021-06-08 16:36:48 +0300

    remove double click with waitButton

--- src/component/button/button.jsx
+++ src/component/button/button.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './button.module.css'
-let click = 0
 
 class Button extends React.Component {
     handleEvent = (purpose) => {
@@ -18,13 +17,7 @@ class Button extends React.Component {
                 break;
             }
             case 'Wait': {
-                click++
-                if (click === 1)
-                    setTimeout(function () {
-                        click = 0;
-                    }, 300);
-                if (click === 2)
-                    this.props.waitTimer();
+                this.props.waitTimer()
                 break;
             }
         }
